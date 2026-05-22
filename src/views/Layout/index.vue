c29e26468ee34101e13d221c7cd516f9b7227c17
luo <2627675225@qq.com>
2026-05-22 14:20:08 +0800

    导航滚动栏的实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,11 +2,13 @@
     import LayoutNav from './components/LayoutNav.vue';
     import LayoutHeader from './components/LayoutHeader.vue';
     import LayoutFooter from './components/LayoutFooter.vue';
+    import LatoutFixed from './components/LayoutFixed.vue'
 </script>
 
 
 <template>
   <div>
+    <LatoutFixed></LatoutFixed>
     <LayoutNav></LayoutNav>
     <LayoutHeader></LayoutHeader>
     <!-- 二级路由出口 -->
